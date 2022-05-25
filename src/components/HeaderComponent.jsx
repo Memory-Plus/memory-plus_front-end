9769eac0c05bed70d80776ae6bf9a8275bd081a2
2cleanwater <2cleanwater@naver.com>
2022-05-25 17:03:37 +0900

    Test : testing header, footer

---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AppBar, Tabs, Tab } from "@material-ui/core";
+
+function HeaderComponent() {
+    return (
+        <div className='header'>
+            
+        </div>
+    );
+}
+
+export default HeaderComponent;
