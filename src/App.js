aaea33276665011fcdb895519870a41709a19dcc
2cleanwater <2cleanwater@naver.com>
2022-05-27 23:01:20 +0900

    Feat : add LoginPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import {BrowserRouter, Route, Switch } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
     return (
       <div className="App">
-        <LoginPage/>
+        <HomePage/>
       </div>
     );
   }
